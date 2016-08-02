f2b7234c5ca2965f1715bbd45a6dd4726ab7b62b
Dmitry Kolesnikov <kastaneda@gmail.com>
2016-08-02 23:13:02 +0300

    Remove dead code

--- src/LanguageBatchBo.php
+++ src/LanguageBatchBo.php
@@ -41,7 +41,6 @@ class LanguageBatchBo
      */
     protected static function getLanguageFile($application, $language)
     {
-        $result = false;
         $languageResponse = ApiCall::call(
             'system_api',
             'language_api',
